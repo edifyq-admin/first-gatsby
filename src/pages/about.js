fe447da4d95b11bb1793b6f221e0f29ceccc7319
Nico Pretorius <nico.pretorius@edifyq.com>
2021-09-26 21:09:22 +0100

    Add about me and layout

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout';
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Me">
+            <p>The about me page</p>
+        </Layout>
+    );
+}
+
+export default AboutPage;
