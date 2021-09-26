1432662bd8fe76a7b284ee0aa786ac5b6e4421ab
edifyq-admin <89722099+edifyq-admin@users.noreply.github.com>
2021-09-26 21:50:58 +0100

    Add plugin (#2)
    * Add about me and layout
    
    * Add image plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
+import { StaticImage } from "gatsby-plugin-image";
 import * as React from "react";
-import { Link } from 'gatsby';
 import Layout from "../components/layout";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>Following the tutorial</p>
+      <StaticImage
+        alt="The beach"
+        src="../images/000.jpg"
+      ></StaticImage>
     </Layout>
   )
 }
