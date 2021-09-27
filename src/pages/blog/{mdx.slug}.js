ec944fc969d600136362b2dafa69ecfbf3dd25db
Nico Pretorius <nico.pretorius@edifyq.com>
2021-09-27 07:00:53 +0100

    Add dynamic image rendering

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -2,11 +2,18 @@ import * as React from 'react';
 import { graphql } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import Layout from '../../components/layout';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 const BlogPost = ({ data }) => {
+    const image = getImage(data.mdx.frontmatter.hero_image);
+
     return (
         <Layout pageTitle={data.mdx.frontmatter.title}>
             <p>{data.mdx.frontmatter.date}</p>
+            <GatsbyImage
+                image={image}
+                alt={data.mdx.frontmatter.hero_image_alt}
+            />
             <MDXRenderer>
                 {data.mdx.body}
             </MDXRenderer>
@@ -18,11 +25,17 @@ export const query = graphql`
     query ($id: String) {
         mdx(id: {eq: $id}) {
             frontmatter {
-              date(formatString: "MMMM D, YYYY")
-              title
+                date(formatString: "MMMM D, YYYY")
+                title
+                hero_image_alt
+                hero_image {
+                    childImageSharp {
+                        gatsbyImageData
+                    }
+                }
             }
             body
-        }      
+        }
     }
 `
 
